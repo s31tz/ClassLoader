eec8f59e06de020f3e1c526e1b0c821d35fc2255
s31tz <fs@fseitz.de>
2017-07-09 18:07:26 +0200

    Savepoint

--- lib/ClassLoader.pm
+++ lib/ClassLoader.pm
@@ -3,7 +3,7 @@ package ClassLoader;
 use strict;
 use warnings;
 
-our $VERSION = 1.115;
+our $VERSION = 1.117;
 
 # -----------------------------------------------------------------------------
 
@@ -309,7 +309,7 @@ werden kann.
 
 =head1 VERSION
 
-1.115
+1.117
 
 =head1 AUTHOR
 
